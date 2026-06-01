7d1e012a3a82bd140a85c68cd545f130011d06fb
podumai <kirillsm05@gmail.com>
2026-06-02 02:52:33 +0700

    feat: add new methods for repository interface

--- internal/domain/task/repository.go
+++ internal/domain/task/repository.go
@@ -2,101 +2,68 @@ package task
 
 import (
 	"context"
-	"errors"
-	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/samber/mo"
 )
 
-var (
-	ErrEmptyFilter = errors.New("empty filter")
-)
-
-type GeoFilter struct {
-	Point  GeoPoint
-	Radius float32
+type FindDeletedFilter struct {
+	OwnerID mo.Option[uuid.UUID]
+	GroupID mo.Option[uuid.UUID]
+	Cursor  Cursor
 }
 
-type Filter struct {
-	OwnerID    mo.Option[uuid.UUID]
-	GroupID    mo.Option[uuid.UUID]
-	IsFavorite mo.Option[bool]
-	Status     mo.Option[Status]
-	Priority   mo.Option[Priority]
-	Area       mo.Option[GeoFilter]
+type FindDeletedFilterBuilder struct {
+	ownerID mo.Option[uuid.UUID]
+	groupID mo.Option[uuid.UUID]
+	cursor  Cursor
 }
 
-func (f *Filter) Validate() error {
-	if f.OwnerID.IsNone() && f.GroupID.IsNone() {
-		return ErrEmptyFilter
+func (b *FindDeletedFilterBuilder) WithOwnerID(id uuid.UUID) *FindDeletedFilterBuilder {
+	b.ownerID = mo.Some(id)
+	return b
+}
+
+func (b *FindDeletedFilterBuilder) WithGroupID(id uuid.UUID) *FindDeletedFilterBuilder {
+	b.groupID = mo.Some(id)
+	return b
+}
+
+func (b *FindDeletedFilterBuilder) WithCursor(cursor Cursor) *FindDeletedFilterBuilder {
+	b.cursor = cursor
+	return b
+}
+
+func (b *FindDeletedFilterBuilder) Build() (FindDeletedFilter, error) {
+	if b.ownerID.IsNone() && b.groupID.IsNone() {
+		return FindDeletedFilter{}, ErrFailedPrecondition
 	}
-	return nil
-}
 
-type FilterBuilder struct {
-	ownerID    mo.Option[uuid.UUID]
-	groupID    mo.Option[uuid.UUID]
-	isFavorite mo.Option[bool]
-	status     mo.Option[Status]
-	priority   mo.Option[Priority]
-	area       mo.Option[GeoFilter]
-}
-
-func (fb *FilterBuilder) SetOwnerID(id uuid.UUID) *FilterBuilder {
-	fb.ownerID = mo.Some(id)
-	return fb
-}
-
-func (fb *FilterBuilder) SetGroupID(id uuid.UUID) *FilterBuilder {
-	fb.groupID = mo.Some(id)
-	return fb
-}
-
-func (fb *FilterBuilder) SetIsFavorite(isFavorite bool) *FilterBuilder {
-	fb.isFavorite = mo.Some(isFavorite)
-	return fb
-}
-
-func (fb *FilterBuilder) SetStatus(status Status) *FilterBuilder {
-	fb.status = mo.Some(status)
-	return fb
-}
-
-func (fb *FilterBuilder) SetPriority(priority Priority) *FilterBuilder {
-	fb.priority = mo.Some(priority)
-	return fb
-}
-
-func (fb *FilterBuilder) SetArea(point GeoPoint, radius float32) *FilterBuilder {
-	fb.area = mo.Some(GeoFilter{
-		Point:  point,
-		Radius: radius,
-	})
-	return fb
-}
-
-func (fb *FilterBuilder) Build() (*Filter, error) {
-	filter := &Filter{
-		OwnerID:    fb.ownerID,
-		GroupID:    fb.groupID,
-		IsFavorite: fb.isFavorite,
-		Status:     fb.status,
-		Priority:   fb.priority,
-		Area:       fb.area,
+	if b.ownerID.IsSome() && b.ownerID.MustGet() == uuid.Nil {
+		return FindDeletedFilter{}, ErrInvalidData
 	}
-	if err := filter.Validate(); err != nil {
-		return nil, fmt.Errorf("validate filter: %w", err)
+
+	if b.groupID.IsSome() && b.groupID.MustGet() == uuid.Nil {
+		return FindDeletedFilter{}, ErrInvalidData
 	}
-	return filter, nil
+
+	return FindDeletedFilter{
+		OwnerID: b.ownerID,
+		GroupID: b.groupID,
+		Cursor:  b.cursor,
+	}, nil
 }
 
-//go:generate mockgen -source=repository.go -destination=mock/mock_repository.go -package=mock
+func NewFindDeletedFilterBuilder() FindDeletedFilterBuilder {
+	return FindDeletedFilterBuilder{}
+}
+
 type Repository interface {
 	Create(ctx context.Context, task *Task) error
 	FindByID(ctx context.Context, id uuid.UUID) (*Task, error)
-	FindAll(ctx context.Context, filter *Filter) ([]*Task, error)
-	FindDeleted(ctx context.Context, filter *Filter) ([]*Task, error)
-	Update(ctx context.Context, task *Task) error
-	Close(ctx context.Context) error
+	FindDeleted(ctx context.Context, filter FindDeletedFilter) ([]uuid.UUID, time.Time, error)
+	Save(ctx context.Context, task *Task) error
+	SoftDelete(ctx context.Context, task *Task) error
+	HardDeleteByUserID(ctx context.Context, id uuid.UUID) error
 }
