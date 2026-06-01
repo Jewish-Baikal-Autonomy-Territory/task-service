b46471018162d8e800f01fbf7fea9b20cf079c1b
podumai <kirillsm05@gmail.com>
2026-06-02 02:39:41 +0700

    refactor: add synchronization key to searcher interface

--- internal/domain/task/searcher.go
+++ internal/domain/task/searcher.go
@@ -2,10 +2,11 @@ package task
 
 import (
 	"context"
+	"time"
 
 	"github.com/google/uuid"
 )
 
 type Searcher interface {
-	Search(ctx context.Context, filter *Filter) ([]uuid.UUID, error)
+	Search(ctx context.Context, filter *Filter) ([]uuid.UUID, time.Time, error)
 }
