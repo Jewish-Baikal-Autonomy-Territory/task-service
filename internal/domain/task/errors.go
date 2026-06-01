085e5f43b02371816da0576805229bcbaf0ea8b3
podumai <kirillsm05@gmail.com>
2026-06-02 02:42:37 +0700

    refactor: add new explicit error for failed precondition

--- internal/domain/task/errors.go
+++ internal/domain/task/errors.go
@@ -3,5 +3,6 @@ package task
 import "errors"
 
 var (
-	ErrNotFound = errors.New("not found")
+	ErrNotFound           = errors.New("not found")
+	ErrFailedPrecondition = errors.New("failed precondition")
 )
