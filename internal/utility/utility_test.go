00d46f9add534dd31572a4f6cf2371eccb3eafd3
podumai <kirillsm05@gmail.com>
2026-06-02 13:31:10 +0700

    test: add table tests for utility package

--- internal/utility/utility_test.go
+++ internal/utility/utility_test.go
@@ -16,23 +16,25 @@ func TestGetEnv(t *testing.T) {
 		want string
 	}{
 		{
-			name: "Missing environment variable",
+			name: "missing environment variable",
 			args: args{
-				key:      "TEST_ENV_MISSING_STR",
-				fallback: "missing-test",
+				key:      "MISSING_ENV_VAR",
+				fallback: "default",
 			},
-			want: "missing-test",
+			want: "default",
 		},
 		{
-			name: "Set environment variable",
+			name: "existing environment variable",
 			args: args{
-				key:      "TEST_ENV_STR",
-				fallback: "missing-test",
+				key:      "ENV_TEST_VAR",
+				fallback: "default",
 			},
-			want: "set-test",
+			want: "env_test_var",
 		},
 	}
-	t.Setenv("TEST_ENV_STR", "set-test")
+
+	t.Setenv("ENV_TEST_VAR", "env_test_var")
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetEnv(tt.args.key, tt.args.fallback); got != tt.want {
@@ -42,7 +44,7 @@ func TestGetEnv(t *testing.T) {
 	}
 }
 
-func TestGetEnvBool(t *testing.T) {
+func TestGetEnvBool1(t *testing.T) {
 	type args struct {
 		key      string
 		fallback bool
@@ -53,22 +55,34 @@ func TestGetEnvBool(t *testing.T) {
 		want bool
 	}{
 		{
-			name: "Missing environment variable",
+			name: "missing environment variable",
 			args: args{
-				key:      "TEST_ENV_MISSING_BOOL",
-				fallback: false,
-			},
-		},
-		{
-			name: "Set environment variable",
-			args: args{
-				key:      "TEST_ENV_BOOL",
-				fallback: false,
+				key:      "MISSING_ENV_VAR",
+				fallback: true,
 			},
 			want: true,
 		},
+		{
+			name: "incorrect environment variable",
+			args: args{
+				key:      "ENV_INCORRECT_VAR",
+				fallback: true,
+			},
+			want: true,
+		},
+		{
+			name: "existing environment variable",
+			args: args{
+				key:      "ENV_TEST_VAR",
+				fallback: false,
+			},
+			want: false,
+		},
 	}
-	t.Setenv("TEST_ENV_BOOL", "true")
+
+	t.Setenv("ENV_INCORRECT_VAR", "some-random-string")
+	t.Setenv("ENV_TEST_VAR", "false")
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetEnvBool(tt.args.key, tt.args.fallback); got != tt.want {
@@ -78,7 +92,7 @@ func TestGetEnvBool(t *testing.T) {
 	}
 }
 
-func TestGetEnvDuration(t *testing.T) {
+func TestGetEnvDuration1(t *testing.T) {
 	type args struct {
 		key      string
 		fallback time.Duration
@@ -89,32 +103,34 @@ func TestGetEnvDuration(t *testing.T) {
 		want time.Duration
 	}{
 		{
-			name: "Missing environment variable",
+			name: "missing environment variable",
 			args: args{
-				key:      "TEST_ENV_MISSING_DURATION",
-				fallback: time.Duration(0),
+				key:      "MISSING_ENV_VAR",
+				fallback: time.Second,
 			},
-			want: time.Duration(0),
+			want: time.Second,
 		},
 		{
-			name: "Incorrect format in environment variable",
+			name: "incorrect environment variable",
 			args: args{
-				key:      "TEST_ENV_INCORRECT_DURATION",
-				fallback: time.Duration(0),
+				key:      "ENV_INCORRECT_VAR",
+				fallback: time.Second,
 			},
-			want: time.Duration(0),
+			want: time.Second,
 		},
 		{
-			name: "Set environment variable",
+			name: "existing environment variable",
 			args: args{
-				key:      "TEST_ENV_DURATION",
-				fallback: time.Duration(0),
+				key:      "ENV_TEST_VAR",
+				fallback: 67*time.Hour + 52*time.Minute,
 			},
-			want: 5 * time.Hour,
+			want: 67*time.Hour + 52*time.Minute,
 		},
 	}
-	t.Setenv("TEST_ENV_INCORRECT_DURATION", "5p")
-	t.Setenv("TEST_ENV_DURATION", "5h")
+
+	t.Setenv("ENV_INCORRECT_VAR", "some-random-string")
+	t.Setenv("ENV_TEST_VAR", "67h52m")
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetEnvDuration(tt.args.key, tt.args.fallback); got != tt.want {
@@ -124,47 +140,145 @@ func TestGetEnvDuration(t *testing.T) {
 	}
 }
 
-func TestGetEnvInt(t *testing.T) {
+func TestGetEnvFloat(t *testing.T) {
 	type args struct {
 		key      string
-		fallback int
+		fallback float64
 	}
 	tests := []struct {
 		name string
 		args args
-		want int
+		want float64
 	}{
 		{
-			name: "Missing environment variable",
+			name: "missing environment variable",
 			args: args{
-				key:      "TEST_ENV_MISSING_INT",
-				fallback: 0,
+				key:      "MISSING_ENV_VAR",
+				fallback: 320.0,
 			},
-			want: 0,
+			want: 320.0,
 		},
 		{
-			name: "Value out of range",
+			name: "incorrect environment variable",
 			args: args{
-				key:      "TEST_ENV_INCORRECT_INT",
-				fallback: 0,
+				key:      "ENV_INCORRECT_VAR",
+				fallback: 120.0,
 			},
-			want: 0,
+			want: 120.0,
 		},
 		{
-			name: "Set environment variable",
+			name: "existing environment variable",
 			args: args{
-				key:      "TEST_ENV_INT",
-				fallback: 0,
+				key:      "ENV_TEST_VAR",
+				fallback: 52.0,
+			},
+			want: 67.0,
+		},
+	}
+
+	t.Setenv("ENV_INCORRECT_VAR", "some-random-string")
+	t.Setenv("ENV_TEST_VAR", "67.0")
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetEnvFloat(tt.args.key, tt.args.fallback); got != tt.want {
+				t.Errorf("GetEnvFloat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetEnvInt1(t *testing.T) {
+	type args struct {
+		key      string
+		fallback int32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int32
+	}{
+		{
+			name: "missing environment variable",
+			args: args{
+				key:      "MISSING_ENV_VAR",
+				fallback: 32,
+			},
+			want: 32,
+		},
+		{
+			name: "incorrect environment variable",
+			args: args{
+				key:      "ENV_INCORRECT_VAR",
+				fallback: 67,
 			},
 			want: 67,
 		},
+		{
+			name: "existing environment variable",
+			args: args{
+				key:      "ENV_TEST_VAR",
+				fallback: 10,
+			},
+			want: 52,
+		},
 	}
-	t.Setenv("TEST_ENV_INCORRECT_INT", "10000000000000000000000000000")
-	t.Setenv("TEST_ENV_INT", "67")
+
+	t.Setenv("ENV_INCORRECT_VAR", "some-random-string")
+	t.Setenv("ENV_TEST_VAR", "52")
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetEnvInt(tt.args.key, tt.args.fallback); got != tt.want {
 				t.Errorf("GetEnvInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetEnvUInt(t *testing.T) {
+	type args struct {
+		key      string
+		fallback uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint32
+	}{
+		{
+			name: "missing environment variable",
+			args: args{
+				key:      "MISSING_ENV_VAR",
+				fallback: 120,
+			},
+			want: 120,
+		},
+		{
+			name: "incorrect environment variable",
+			args: args{
+				key:      "ENV_INCORRECT_VAR",
+				fallback: 52,
+			},
+			want: 52,
+		},
+		{
+			name: "existing environment variable",
+			args: args{
+				key:      "ENV_TEST_VAR",
+				fallback: 100,
+			},
+			want: 67,
+		},
+	}
+
+	t.Setenv("ENV_INCORRECT_VAR", "some-random-string")
+	t.Setenv("ENV_TEST_VAR", "67")
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetEnvUInt(tt.args.key, tt.args.fallback); got != tt.want {
+				t.Errorf("GetEnvUInt() = %v, want %v", got, tt.want)
 			}
 		})
 	}
